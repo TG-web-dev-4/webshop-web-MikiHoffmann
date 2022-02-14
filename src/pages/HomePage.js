936c926c2febb6ddfdb6cc449bc583b2b525b0b8
Miki Hoffmann <mh.miki.hoffmann@gmail.com>
2022-02-14 16:43:44 +0100

    new products

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,18 +5,9 @@ import { StyledArticle } from "../styles/styledComponents/Article.styled";
 import ProductCard from "../components/ProductCard";
 
 const HomePage = ({ products }) => {
-  const backGround = document.getElementById("backGround");
-  console.log(backGround);
-  window.addEventListener("scroll", function () {
-    backGround.style.transform =
-      "background-position(" + window.pageYOffset / 4 + "deg)";
-  });
   //console.log(allProducts[0]);
   return (
     <StyledMain>
-      <div id="backGround" className="backGround">
-        hello
-      </div>
       <StyledArticle>
         <h2>homepage</h2>
         <div>
